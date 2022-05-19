6d850faefd17d33a78170a03d0c3b3b5292d9dd0
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-05-19 21:36:38 +1000

    added dismissable behaviour

--- web/pages/index.js
+++ web/pages/index.js
@@ -5,46 +5,52 @@ import {
   SpeakerphoneIcon,
   XIcon,
 } from "@heroicons/react/outline";
+import { useState } from "react";
 
 export default function Home() {
+  const [visible, setVisibility] = useState(true);
+
   return (
     <>
-      <div className="bg-cyan-600">
-        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between flex-wrap">
-            <div className="w-0 flex-1 flex items-center">
-              <span className="flex p-2 rounded-lg bg-cyan-800">
-                <SpeakerphoneIcon
-                  className="h-6 w-6 text-white"
-                  aria-hidden="true"
-                />
-              </span>
-              <p className="ml-3 font-medium text-white truncate">
-                Sign up to know when we launch!
-              </p>
-            </div>
-            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-              <a
-                href="https://airtable.com/shr1SWVIGZt30Ig0y"
-                target="_blank"
-                rel="noreferrer"
-                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-cyan-600 bg-white hover:bg-cyan-50"
-              >
-                Sign up here
-              </a>
-            </div>
-            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-              <button
-                type="button"
-                className="-mr-1 flex p-2 rounded-md hover:bg-cyan-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-              >
-                <span className="sr-only">Dismiss</span>
-                <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
-              </button>
+      {visible && (
+        <div className="bg-cyan-600">
+          <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between flex-wrap">
+              <div className="w-0 flex-1 flex items-center">
+                <span className="flex p-2 rounded-lg bg-cyan-800">
+                  <SpeakerphoneIcon
+                    className="h-6 w-6 text-white"
+                    aria-hidden="true"
+                  />
+                </span>
+                <p className="ml-3 font-medium text-white truncate">
+                  Sign up to know when we launch!
+                </p>
+              </div>
+              <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                <a
+                  href="https://airtable.com/shr1SWVIGZt30Ig0y"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-cyan-600 bg-white hover:bg-cyan-50"
+                >
+                  Sign up here
+                </a>
+              </div>
+              <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+                <button
+                  type="button"
+                  onClick={() => setVisibility(false)}
+                  className="-mr-1 flex p-2 rounded-md hover:bg-cyan-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+                >
+                  <span className="sr-only">Dismiss</span>
+                  <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       <header className="py-4 max-w-xs mx-auto">
         <div className="text-cyan-600 font-bold text-lg">Polipal</div>
       </header>
